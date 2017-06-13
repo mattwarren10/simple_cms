6d93028f862409446e3282852cfcc89b4a670ed0
Matt Warren <mwarren@harding.edu>
2017-06-13 10:45:50 -0400

    fix pages controller. include subject_id within views

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,5 +3,11 @@ class Page < ApplicationRecord
 	belongs_to :subject, { :optional => false }
 	has_many :sections
 	has_and_belongs_to_many :admin_users
+
+	scope :visible, lambda { where(:visible => true) }
+	scope :invisible, lambda { where(:visible => false) }
+	scope :sorted, lambda { order(:position => :asc) }
+	scope :newest_first, lambda { order(:created_at => :desc) }
+	scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"])}
 	
 end
