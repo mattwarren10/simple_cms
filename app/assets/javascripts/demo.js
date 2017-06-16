fdd545802c613db8ad6016563c70b37b99c65b79
Matt Warren <mwarren@harding.edu>
2017-06-16 13:34:24 -0400

    add jquery gem. add gradient to primary.css.scss.

---
+++ app/assets/javascripts/demo.js
@@ -0,0 +1,7 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+function msg(name) {
+	alert('This is an alert message for' + name)
+}
