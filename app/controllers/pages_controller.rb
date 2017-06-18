703f36bed7d44bdef85238ad34d1cf459d74f418
Matt Warren <mwarren@harding.edu>
2017-06-18 14:17:04 -0400

    nest pages within subjects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,20 @@ class PagesController < ApplicationController
   layout 'admin'
 
   before_action :confirmed_logged_in
+  before_action :find_subject
   before_action :find_subjects, :only => [:new, :create, :edit, :update]
   before_action :find_page, :except => [:index, :new, :create]
   before_action :set_page_count, :only => [:new, :create, :edit, :update]
   
   def index
-    @pages = Page.sorted
+    @pages = @subject.pages.sorted
   end
 
   def create
     @page = Page.new(pages_params)
     if @page.save
       flash[:notice] = 'Page created successfully.'
-      redirect_to(page_path(@page))
+      redirect_to(pages_path(:subject_id => @subject.id))
     else          
       render :new
     end
@@ -32,20 +33,20 @@ class PagesController < ApplicationController
   def destroy
     @page.destroy
     flash[:notice] = "Page '#{@page.name}' destroyed successfully."
-    redirect_to(pages_path)
+    redirect_to(pages_path(:subject_id => @subject.id))
   end
 
   def update
     if @page.update_attributes(pages_params)
       flash[:notice] = 'Page updated successfully.'
-      redirect_to(page_path(@page))
+      redirect_to(page_path(@page, :subject_id => @subject.id))
     else           
       render :edit
     end
   end
 
   def new
-     @page = Page.new 
+     @page = Page.new(:subject_id => @subject.id)
   end
 
   def edit
@@ -56,6 +57,10 @@ class PagesController < ApplicationController
 
   def pages_params
     params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+  end
+
+  def find_subject
+    @subject = Subject.find(params[:subject_id])
   end
 
   def find_subjects
