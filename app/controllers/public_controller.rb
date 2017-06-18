f7518a153646b0272b976e2163866f8c1aec93c8
Matt Warren <mwarren@harding.edu>
2017-06-18 14:00:39 -0400

    add navigation partial to public.html.erb. create before_action for it.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,8 @@ class PublicController < ApplicationController
 
   layout 'public'
 
+  before_action :setup_navigation
+
   def index
   	#intro text
   end
@@ -13,5 +15,10 @@ class PublicController < ApplicationController
   	else
   		#display page content using show.html.erb
   	end
+  end
+
+  private
+  def setup_navigation
+  	@subjects = Subject.visible.sorted
   end
 end
