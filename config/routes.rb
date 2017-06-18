363a37d977b599765d48027e91798eedc8a1f567
Matt Warren <mwarren@harding.edu>
2017-06-18 13:51:09 -0400

    g controller public. add routes and index and show actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'demo#index'
+  root :to => 'public#index'
+  
+
+  get 'show/:permalink', :to => 'public#show'
+
 
   get 'admin', :to => 'access#menu'
   get 'access/menu'
